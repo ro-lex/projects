e34c84006d6e720c08958ccb7eeacc91bc5cf310
ro-lex <swift.rolex@gmail.com>
2016-10-14 21:00:29 -0500

    Creating sample maven archetype generator projet

---
+++ learner-projects/create-maven-archetype/src/main/resources/archetype-resources/src/main/java/TemplateApp.java
@@ -0,0 +1,13 @@
+package $package;
+
+/**
+ * Hello world!
+ *
+ */
+public class TemplateApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Template App World!" );
+    }
+}
---
+++ learner-projects/create-maven-archetype/src/main/resources/archetype-resources/src/main/java/__artifactId__/__artifactId__.java
@@ -0,0 +1,13 @@
+package $package;
+
+/**
+ * Hello world!
+ *
+ */
+public class $artifactId
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Parameter Testing!" );
+    }
+}
